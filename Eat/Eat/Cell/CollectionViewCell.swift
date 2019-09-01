bb90548b439525b6bb5659fb6b518c6fbc74924f
Priyanka Chaturvedi <priyankachaturvedi19@gmail.com>
2019-08-31 19:07:58 -0500

    Horizontal collection view image loaded

---
+++ Eat/Eat/Cell/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Eat
+//
+//  Created by Manish Chaturvedi on 8/31/19.
+//  Copyright © 2019 Manish Chaturvedi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var restImageView: UIImageView!
+}
